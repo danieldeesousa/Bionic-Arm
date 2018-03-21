b6353e515369185e74f81b4dd12a1f1a7c8e1b1b
danieldeesousa <daniel-desousa_22@hotmail.com>
2018-03-21 03:49:28 -0400

    Added deterministic function + Motor noise removal

--- BME361.ino
+++ BME361.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-Servo servo;           // servo instance
+Servo servo;                                    // servo instance
 
 // Pin definitions
 #define read_flex 0                             // ANALOG - flex sensor reading
@@ -21,8 +21,21 @@ volatile unsigned long last_valid;              // In useconds
 volatile bool state = 0;                        // 1: ON 0: LOW POWER
 volatile bool int_flag = 0;                     // used to signal whem button was just pressed
 
-// Flex sensor and filters
+// Flex sensor processing and filters
 int flexSensor;                                 // analog read of flexsensor
+bool gripState = 0;                             // 0 = no grip; 1 = grip (motor @ 180)
+// - gripping
+const int grip_threshold = 800;                 // flex must exceed this val to be considered gripping
+long grip_startTime = 0;                        // used to determine time duration which grip has beem on
+// - releasing                                  -----------------------------------------------------------
+uint8_t rel_count = 0;                          // number of twitches in time frame
+int rel_threshold = 25;                         // difference between valley value and flex at grip
+long rel_startTime = 0;                         // used to determine how many finger twitches in time frame
+int flex_at_grip = 0;                           // flex value recorded when entering grip mode
+float rel_prevVal = 0;                          // used in valley detection
+float rel_currVal = 0;                          // used in valley detection
+float rel_nextVal = 0;                          // used in valley detection
+// - filtering                                  -----------------------------------------------------------
 float iir_Av = 0;                               // used to track IIR value
 int16_t cbuf[32];                               // buffer storing latest 32 values
 uint8_t offset = 0;
@@ -40,33 +53,111 @@ void setup()
 
 void loop() 
 {
-  // read sensor value (voltage divider - 10K)
-  flexSensor = analogRead(read_flex);
+  // If in normal power mode
+  if(state)
+  {
+    // read sensor value (voltage divider - 10K) and filter
+    flexSensor = analogRead(read_flex);
+    float iirStatus = iirFilter(flexSensor);
+    
+    // determine whether device should grip or ungrip
+    bool isGrip; // Grip; 1 = Y
+    bool relGrip; // Release; 1 = Y
+    if(!gripState)
+      isGrip = forceGrip(iirStatus);
+    else if(gripState)
+      relGrip = releaseGrip(iirStatus);
+    if(isGrip && !gripState)
+    {
+      gripState = 1; // gripped
+      driveMotor(180);
+    }
+    else if(relGrip && gripState)
+    {
+      gripState = 0; // unreleased grip
+      driveMotor(0);
+    }
+  }
 
-  // calculate filter values
-  float iirStatus = iirFilter(flexSensor);
-  batt_state = checkBattery();
-  changeState();
-
-  // check if interrupt or low battery state or detected grip/release
+   // Update states
+   batt_state = checkBattery();
+   changeState();
+  
+  // SAFETY::: check if button pressed (interrupt) or low battery state
   if(int_flag || !batt_state)
   {
-    if(!batt_state || !state)
-      driveMotor(0);
-    else
-      driveMotor(180);
+    driveMotor(0);
+    gripState = 0;
     int_flag = 0;
   }  
 }
 
+// FUNCTION: Drive linear motor
 void driveMotor(int servoPos)
 {
   // move servo bus restrict to (0,180)
   if(servoPos <= 180 && servoPos >= 0)
     servo.write(servoPos);
+  delay(3000); // used for motor noise removal
 }
 
-// Low Pass IIR Filter
+// FUNCTION: determines whether user wishes to enhance grip
+// Operation: flex value must be kept above threshold for certain amount of time
+bool forceGrip(float flexVal)
+{
+  if(flexVal >= grip_threshold)
+  {
+    if(grip_startTime == 0)
+    {
+      grip_startTime = millis();
+    }
+    if((millis() - grip_startTime) >= 5000)
+    {
+      flex_at_grip = flexVal; // store current flex value - used in release func
+      grip_startTime = 0;
+      return true;
+    }
+  }
+  // Broke condition - restart
+  else
+    grip_startTime = 0;
+  return false;
+}
+
+// FUNCTION: determines whether user wishes to release grip
+// Operation: detect 3 valleys in flex of a certain threshold
+bool releaseGrip(float flexVal)
+{
+  // Update values for peak detection
+  rel_prevVal = rel_currVal;
+  rel_currVal = rel_nextVal;
+  rel_nextVal = flexVal;
+  // Check if valley and if it meets threshold
+  if(rel_currVal <= rel_prevVal && rel_currVal <= rel_nextVal && (flex_at_grip - rel_currVal) >= rel_threshold)
+  {
+    // If first valley, start the counter
+    if(rel_count == 0)
+    {
+      rel_startTime = millis();
+    }
+    rel_count++;
+    // If 3 valleys counted within time frame
+    if((rel_count >= 3) && ((millis() - rel_startTime) <= 5000))
+    {
+      rel_startTime = 0;
+      rel_count = 0;
+      return true;
+    }
+  }
+  // If time frame expires
+  if((millis() - rel_startTime) >= 5000)
+  {
+    rel_count = 0;
+  }
+  return false;
+}
+
+// FUNCTION: Low Pass IIR Filter
 float iirFilter(int flexVal)
 {
   //iir_Av += (((float)flexVal << 15) - iir_Av) >> 4);
@@ -74,7 +165,7 @@ float iirFilter(int flexVal)
   return iir_Av;
 }
 
-//  Low Pass FIR Filter (12 pole)
+// FUNCTION: Low Pass FIR Filter (12 pole)
 int lowPassFIRFilter(int newSample)
 {  
   // store new value in array
@@ -97,12 +188,13 @@ int lowPassFIRFilter(int newSample)
   return(z >> 15);
 }
 
+// FUNCTION: Used to turn on tricolour LED
 void turnOnLED(int ledColour)
 {
   PORTB = (1 << (ledColour - 8));
 }
 
-// change state from ON <-> LOW POWER
+// FUNCTION: State change between ON, Low power, Low battery
 void changeState()
 {
   if(!batt_state)
@@ -116,6 +208,7 @@ void changeState()
     turnOnLED(LED_BLUE);
 }
 
+// FUNCTION: Check state of battery
 bool checkBattery()
 {
 // read value from sensor and divide it by the resolution, multiply by reference voltage
@@ -126,7 +219,7 @@ bool checkBattery()
     return false;
 }
 
-// Debouncing function - determines whether button press is valid
+// FUNCTION: Debouncing function - determines whether button press is valid
 void ISR_debounce()
 {
   if((long)(micros() - last_valid) >= debouncing_time) 
@@ -137,7 +230,7 @@ void ISR_debounce()
   }
 }
 
-//  Integer multiplier
+// FUNCTION: Integer multiplier
 int32_t mul16(int16_t x, int16_t y)
 {
   return((long)x * (long)y);
